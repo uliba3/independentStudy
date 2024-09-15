f3a6fedf80937adbf86ffa195f259a83dc3263e8
Soobin Yang <yasoo8449@gmail.com>
2024-09-15 10:16:40 -0400

    remove unnecessary files

--- saveJson.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def load_json(filename):
-    with open(filename, 'r') as file:
-        return json.load(file)
-
-def save_json(data, filename):
-    with open(filename, 'w') as file:
-        json.dump(data, file)
